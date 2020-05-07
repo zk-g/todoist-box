9ac540bca15b764f51a547c2dd6d947d3eba929a
yg <61746440+yg@users.noreply.github.com>
2020-05-07 12:01:28 +0000

    Fix Octokit Error

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 require("dotenv").config();
-const Octokit = require("@octokit/rest");
+const { Octokit } = require("@octokit/rest");
 const axios = require("axios");
-const humanize = require('humanize-number');
+const humanize = require("humanize-number");
 
 const {
   GIST_ID: gistId,
   GH_TOKEN: githubToken,
-  TODOIST_API_KEY: todoistApiKey
+  TODOIST_API_KEY: todoistApiKey,
 } = process.env;
 
 const octokit = new Octokit({ auth: `token ${githubToken}` });
 
 async function main() {
-  const stats = await axios(`https://api.todoist.com/sync/v8.3/completed/get_stats?token=${todoistApiKey}`);
+  const stats = await axios(
+    `https://api.todoist.com/sync/v8.3/completed/get_stats?token=${todoistApiKey}`
+  );
   await updateGist(stats.data);
 }
 
@@ -25,43 +27,37 @@ async function updateGist(data) {
   }
 
   const lines = [];
-  const {
-    karma,
-    completed_count,
-    days_items,
-    week_items,
-    goals
-  } = data;
-  
-  const karmaPoint = [
-    `🏆 ${humanize(karma)} Karma Points`
-  ];
+  const { karma, completed_count, days_items, week_items, goals } = data;
+
+  const karmaPoint = [`🏆 ${humanize(karma)} Karma Points`];
   lines.push(karmaPoint.join(" "));
 
   const dailyGoal = [
-    `🌞 Completed ${days_items[0].total_completed.toString()} tasks out of ${goals.daily_goal} today`
+    `🌞 Completed ${days_items[0].total_completed.toString()} tasks out of ${
+      goals.daily_goal
+    } today`,
   ];
   lines.push(dailyGoal.join(" "));
 
   const weeklyGoal = [
-    `📅 Completed ${week_items[0].total_completed.toString()} tasks out of ${goals.weekly_goal} this week`
+    `📅 Completed ${week_items[0].total_completed.toString()} tasks out of ${
+      goals.weekly_goal
+    } this week`,
   ];
   lines.push(weeklyGoal.join(" "));
 
-  const totalTasks = [
-    `✅ Completed ${humanize(completed_count)} tasks so far`
-  ];
+  const totalTasks = [`✅ Completed ${humanize(completed_count)} tasks so far`];
   lines.push(totalTasks.join(" "));
 
   const longestStreak = [
-    `⌛ Longest streak is ${humanize(goals.last_daily_streak.count)} days`
+    `⌛ Longest streak is ${humanize(goals.last_daily_streak.count)} days`,
   ];
   lines.push(longestStreak.join(" "));
 
   if (lines.length == 0) return;
 
   try {
-    console.log(lines.join("\n"))
+    console.log(lines.join("\n"));
     // Get original filename to update that same file
     const filename = Object.keys(gist.data.files)[0];
     await octokit.gists.update({
@@ -69,9 +65,9 @@ async function updateGist(data) {
       files: {
         [filename]: {
           filename: `✅ Todoist Stats`,
-          content: lines.join("\n")
-        }
-      }
+          content: lines.join("\n"),
+        },
+      },
     });
   } catch (error) {
     console.error(`Unable to update gist\n${error}`);
